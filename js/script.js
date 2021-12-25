2cdabb09dbdd059c1dc0c5cd09bf0af1f2f7e290
Oleg <ollegozz@yandex.ru>
2021-12-25 11:41:10 +0300

    комментарии

--- js/script.js
+++ js/script.js
@@ -1,22 +1,25 @@
-const dino = document.getElementById ("dino");
+const dino = document.getElementById ("dino");  //получаем элемент
 const cactus = document.getElementById("cactus");
 
-document.addEventListener("keydown", function(event) {
-    jump();
+document.addEventListener("keydown", function(event) {  //обработчик событый со значением нажатие кнопки 
+    jump();  //функия добавляет класс элементу
 }
 );
 
 function jump() {
-    if (dino.classList != "jump") {
-        dino.classList.add("jump")
+    if (dino.classList != "jump") {  //возвращает все классы элемента
+        dino.classList.add("jump")  //если дино не имеет ккласс джамп - добавляем
     }
-    setTimeout(function () {
+    setTimeout(function () {  // или удаляем, проверка 300мс
         dino.classList.remove("jump")
     }, 300)
 }
 
-let iaAlive = setInterval ( function () {
-    let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
+let iaAlive = setInterval ( function () {  // вызов через опред 10мс интервал времени 
+    let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));  
+    //получаем значение где находится дино getComputedStyle(dino) из scc свойств
+    //getPropertyValue получем свойство, которое должно быть обработано, тип строковое
+    //parseInt переводим строку в int
     let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"));
 
     if (cactusLeft <50 && cactusLeft > 0 && dinoTop >=140) {
